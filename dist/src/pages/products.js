26d1129dedd25a32b7c84b8c0d5d2dca0db3eccf
VladiVK <kozobon@gmail.com>
2021-05-31 16:03:58 +0200

    setup Products page html and css, data for products container

--- dist/src/pages/products.js
+++ dist/src/pages/products.js
@@ -1,2 +1,12 @@
 import '../cart/toggleCart.js';
 import '../toggleSidebar.js';
+
+import { store } from '../store.js';
+import display from '../displayProducts.js';
+
+import { getElement } from '../utils.js';
+
+const loadingDOM = getElement('.page-loading');
+
+display(store, getElement('.products__container'));
+loadingDOM.style.display = 'none';
