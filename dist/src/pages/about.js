817898efae422e226eabd308671e16971bad8116
VladiVK <kozobon@gmail.com>
2021-06-02 00:02:02 +0200

    About Page: html, css, js

---
+++ dist/src/pages/about.js
@@ -0,0 +1,25 @@
+// global imports
+import '../cart/toggleCart.js';
+import '../toggleSidebar.js';
+
+const questionTitles = document.querySelectorAll('.question__title');
+
+questionTitles.forEach((title) => {
+  title.addEventListener('click', () => {
+    //   .question__text-container
+    const textContainer = title.nextElementSibling;
+
+    if (textContainer.classList.contains('active-text')) {
+      textContainer.classList.remove('active-text');
+    } else {
+      const text = textContainer.firstElementChild;
+
+      const containerHeight = textContainer.getBoundingClientRect().height;
+      const textHeight = text.getBoundingClientRect().height;
+
+      containerHeight === 0
+        ? (textContainer.style.height = `${textHeight}px`)
+        : (textContainer.style.height = 0);
+    }
+  });
+});
