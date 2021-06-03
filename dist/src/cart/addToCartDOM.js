2352e16ea312113a343b8097019ac65bed1d3002
VladiVK <kozobon@gmail.com>
2021-06-03 22:28:23 +0200

    Cart: single-cart-item CSS

---
+++ dist/src/cart/addToCartDOM.js
@@ -0,0 +1,23 @@
+//   <article class="cart__item">
+//     <img
+//       class="cart__item--img"
+//       src="./images/main-bg_primary.jpg"
+//       alt=""
+//     />
+
+//     <div>
+//       <h4 class="cart__item--name">name</h4>
+//       <p class="cart__item--price">$100</p>
+//       <button class="cart__item--remove-btn" data-id="1">remove</button>
+//     </div>
+
+//     <div>
+//       <button class="cart__item--increase-btn" data-id="1">
+//         <i class="fas fa-chevron-up"></i>
+//       </button>
+//       <p class="cart__item--amount" data-id="1">$100</p>
+//       <button class="cart__item--decrease-btn" data-id="1">
+//         <i class="fas fa-chevron-down"></i>
+//       </button>
+//     </div>
+//   </article>
